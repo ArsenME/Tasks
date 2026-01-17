49bc1a275fdfec62facf0faf0f4ab9068116f196
ArsenME <melikyanarsen190@gmail.com>
2026-01-17 22:59:34 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,279 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+//    Scanner scanner1 = new Scanner(System.in);
+//
+//              System.out.println("Enter an integer: ");
+//        int a = scanner1.nextInt();
+//
+//              System.out.println("Enter an integer: ");
+//        int b = scanner1.nextInt();
+
+//              System.out.println("Enter an integer: ");
+//        int c = scanner1.nextInt();
+        //21
+//       if(b > a){
+//
+//           a = b;
+//       }
+//       if (c > a){
+//           a = c;
+//
+//       }
+//        System.out.println(a);
+
+        //23
+//        boolean isOne = false;
+//        if(a == 1 || b == 1 || c == 1){
+//                isOne = true;
+//        }
+//        System.out.println(isOne);
+
+
+
+        //25
+//        short y = 2;
+//        int z =a;
+//        if(b > a){
+//
+//            a = b;
+//            b = z;
+//     }
+//     if (c > a){
+//        z = a;
+//         a = c;
+//         c = z;
+//
+//       }
+//     if(a< b+c){
+//
+//       y= 1;
+//     }
+//
+//        System.out.println(y);
+//
+     //27
+//        boolean check = false;
+//        if(b-a == c-b){
+//            check = true;
+//
+//        }
+//        System.out.println(check);
+
+
+
+
+
+       //29
+//        boolean check = false;
+//    if(b - a > 0){
+//        if(c - b > 0){
+//            check = true;
+//
+//        }
+//    }
+//        System.out.println(check);
+
+
+//        Scanner scanner2 = new Scanner(System.in);
+//        System.out.println("a: ");
+//                int a = scanner2.nextInt();
+//                     System.out.println("b: ");
+//                int b = scanner2.nextInt();
+//                    System.out.println("c: ");
+//                int c = scanner2.nextInt();
+//                    System.out.println("d: ");
+//                int d  = scanner2.nextInt();
+        //31
+//        if(b > a){
+//            a = b;
+//
+//        }
+//        if(c > a){
+//            a = c;
+//        }
+//        if(d > a){
+//            a = d;
+//        }
+//        System.out.println(a);
+
+
+        //33
+//        boolean check = false;
+//
+//        if((a == 1 || b == 1 || c == 1 || d == 1){
+//            check = true;
+//        }
+//
+//        System.out.println(check);
+
+
+        //35
+//        int y = a ;
+//        boolean check = false;
+//        if(b > a){
+//            a = b;
+//            b =y;
+//        }
+//        if(c > a){
+//            y = a;
+//            a = c;
+//            c = y;
+//        }
+//        if(d > a){
+//            y = a;
+//            a = d;
+//            d = y;
+//        }
+//        if(a == b+c+d){
+//            check = true;
+//
+//        }
+//        System.out.println(check);
+//        check = false;
+//        int sum = a+b+c+d;
+//        if(sum / 2 == a || sum / 2 == b || sum / 2 == c || sum / 2 == d){
+//            check = true;
+//
+//        }
+//        System.out.println(check);
+
+        //37
+//        boolean check = false;
+//        int dis = b - a;
+//        if(d - c == dis  ){
+//            if(c - b == dis){
+//                check = true;
+//
+//
+//            }
+//        }
+//        System.out.println(check);
+
+
+        //39
+
+//        int[] arr ={a,b,c,d};
+//        int temp = 0;
+//        for(int i = 0 ; i < 4; i++){
+//
+//            for(int j = i+1; j < 4; j++)
+//            {
+//                if(arr[i] > arr[j]) {
+//                    temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                }
+//
+//
+//
+//                    System.out.print("[");
+//                    for(int g = 0; g < 4;g++){
+//                        System.out.printf("%d",arr[g]);
+//
+//                    }
+//                    System.out.println("]");
+//                }
+//
+//        }
+//
+//
+         Scanner  scanner3 = new Scanner(System.in);
+        System.out.println("Enter number");
+       int number = scanner.nextInt();
+
+
+       //51
+//        boolean t = false;
+//            int a = number%10;
+//        System.out.println(a);
+//            int b = (number /10)%10;
+//        System.out.println(b);
+//            int c = number / 100;
+//        System.out.println(c);
+//            if(a == b + c){
+//                t= true;
+//
+//            }
+//        System.out.println(t);
+
+
+
+        //53
+//            int a = number%10;
+//
+//            int b = (number /10)%10;
+//
+//            int c = number / 100;
+//
+//
+//        System.out.println("Enter the k number");
+//            int k = scanner3.nextInt();
+//             int equal =number/a;
+//            if(number > k){
+//                equal = number/(a+b+c);
+//            }
+//        System.out.println(equal);
+
+
+        //55
+
+//        int a = number%10;
+//
+//        int b = (number /10)%10;
+//
+//        int c = number / 100;
+//            if(a > b){
+//
+//                a = b ;
+//            }
+//            if(a > c){
+//
+//                a = c;
+//            }
+//        System.out.println(a);
+
+
+            //57//
+
+
+//        int a = number%10;
+//
+//        int b = (number /10)%10;
+//
+//        int c = number / 100;
+
+
+//                                                    for(;;){
+//                                                        if(a == 0){
+//                                                            System.out.println("Enter number");
+//                                                             number = scanner.nextInt();
+//
+//                                                        }
+//                                                        else{
+//                                                            if(number > 300){
+//                                                                float result = b/a;
+//                                                                System.out.println(result);
+//                                                            }
+//                                                            else {
+//                                                                float result = c/a;
+//                                                                System.out.println(result);
+//                                                            }
+//
+//
+//                                                        }
+//                                                    }
+
+
+
+
+    }
+
+
+
+
+
+
+
+}
